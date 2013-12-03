66bd69de854e77ef4e376e68424201685efecb6c
Matthew Weier O'Phinney <matthew@zend.com>
2013-12-03 15:00:12 -0600

    zendframework/zf2@148a0b486072c722323d95a91cee2c9a0e85cabb (1386103253)

--- Adapter/Driver/Pdo/Connection.php
+++ Adapter/Driver/Pdo/Connection.php
@@ -46,11 +46,6 @@ class Connection implements ConnectionInterface, Profiler\ProfilerAwareInterface
     protected $inTransaction = false;
 
     /**
-     * @var string
-     */
-    protected $dsn = null;
-
-    /**
      * Constructor
      *
      * @param array|\PDO|null $connectionParameters
@@ -138,20 +133,6 @@ class Connection implements ConnectionInterface, Profiler\ProfilerAwareInterface
     public function getConnectionParameters()
     {
         return $this->connectionParameters;
-    }
-
-    /**
-     * Get the dsn string for this connection
-     * @throws \Zend\Db\Adapter\Exception\RunTimeException
-     * @return string
-     */
-    public function getDsn()
-    {
-        if (!$this->dsn) {
-            throw new Exception\RunTimeException("The DSN has not been set or constructed from parameters in connect() for this Connection");
-        }
-
-        return $this->dsn;
     }
 
     /**
@@ -259,9 +240,6 @@ class Connection implements ConnectionInterface, Profiler\ProfilerAwareInterface
                 case 'dbname':
                     $database = (string) $value;
                     break;
-                case 'charset':
-                    $charset    = (string) $value;
-                    break;
                 case 'driver_options':
                 case 'options':
                     $value = (array) $value;
@@ -279,14 +257,6 @@ class Connection implements ConnectionInterface, Profiler\ProfilerAwareInterface
                 case 'sqlite':
                     $dsn[] = $database;
                     break;
-                case 'sqlsrv':
-                    if (isset($database)) {
-                        $dsn[] = "database={$database}";
-                    }
-                    if (isset($hostname)) {
-                        $dsn[] = "server={$hostname}";
-                    }
-                    break;
                 default:
                     if (isset($database)) {
                         $dsn[] = "dbname={$database}";
@@ -297,9 +267,6 @@ class Connection implements ConnectionInterface, Profiler\ProfilerAwareInterface
                     if (isset($port)) {
                         $dsn[] = "port={$port}";
                     }
-                    if (isset($charset)) {
-                        $dsn[] = "charset={$charset}";
-                    }
                     break;
             }
             $dsn = $pdoDriver . ':' . implode(';', $dsn);
@@ -309,8 +276,6 @@ class Connection implements ConnectionInterface, Profiler\ProfilerAwareInterface
                 $this->connectionParameters
             );
         }
-
-        $this->dsn = $dsn;
 
         try {
             $this->resource = new \PDO($dsn, $username, $password, $options);
@@ -469,5 +434,4 @@ class Connection implements ConnectionInterface, Profiler\ProfilerAwareInterface
         }
         return false;
     }
-
 }
